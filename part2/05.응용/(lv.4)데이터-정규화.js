d0d173189b3e108212f1a2c4cb42a0fdd99c4068
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-02-21 09:55:27 +0900

    고용준 21일차 문제은행 풀이 제출

--- part2/05.응용/(lv.4)데이터-정규화.js
+++ part2/05.응용/(lv.4)데이터-정규화.js
@@ -28,7 +28,16 @@
  */
 
 // TODO: normalizeData 함수를 작성하세요.
-function normalizeData(data) {}
+function normalizeData(data) {
+  return data.reduce(
+    (acc, item) => {
+      acc.byId[item.id] = item;
+      acc.allIds.push(item.id);
+      return acc;
+    },
+    { byId: {}, allIds: [] }
+  );
+}
 
 // export를 수정하지 마세요.
 export { normalizeData };
